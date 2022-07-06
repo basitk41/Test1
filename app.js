b01cb55647dabada627ab9a876a5110454aa8ec8
sangraiz <healermoebius647@gmail.com>
2022-07-06 10:23:39 +0500

    exercise no 1 swapping two values

---
+++ app.js
@@ -0,0 +1,13 @@
+// Exercise No1 (swapping of two values)
+let a = 10;
+let b = 20;
+console.log('Before the swap');
+console.log('A = ',a);
+console.log('B = ',b);
+let c;
+c = a; // now c = 10,a = 10 and b = 20
+a = b; // now c = 10,a = 20 and b = 20
+b = c; // now c = 10,a = 10 and b = 10
+console.log('After  swapping');
+console.log('A = ',a);
+console.log('B = ',b);
