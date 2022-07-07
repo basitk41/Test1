c65234b4dbc9bd73f1480ce633571688c487f55b
Basit Ali <basitk41@gmail.com>
2022-07-07 11:52:59 +0500

    exercises

---
+++ index.js
@@ -0,0 +1,27 @@
+// const arr = [];
+
+// for (var i = 1; i <= 10; i++) {
+//   var func = (num) => () => console.log(num);
+//   arr.push(func(i));
+//   //   arr.push(()=>console.log(i));
+// }
+
+// arr.forEach((el) => {
+//   el();
+// });
+
+///////////////////////////////////////////////
+// checkAge(age) => age <= 12 => "child" => age >= 12 && age <= 18 => "teenager" => age >= 18 => "adult"
+// const checkAge = (age) => {
+//   return age <= 12 ? "child" : age >= 12 && age <= 18 ? "teenager" : "adult";
+// };
+// console.log(checkAge(12));
+
+/////////////////////////////////////////////
+// stack => FILO
+// queue => FIFO
+
+// length => stack.length
+const stack = [];
+const push = (value) => {};
+const pop = () => {};
