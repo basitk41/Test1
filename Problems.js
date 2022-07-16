6e333dbedfaed7b518928805bce69d64d7255a9a
sangraiz <healermoebius647@gmail.com>
2022-07-16 15:26:21 +0500

    *

--- Problems.js
+++ Problems.js
@@ -197,17 +197,14 @@
 
 // ----------------------------- //
 // Exercise 12
-// let string = "";
-// for (let i = 0; i < 5; i++) {
-//    for (let j = 0; j <= i; j++) {
-//         string += '*';
-//    }
-//    for (let k = 0; k <= i-1; k++) {
-//         string += ' ';
-//    }
-//    string += '\n';
-// }
-// console.log(string);
+let string = "";
+for (let i = 0; i < 5; i++) {
+   for (let j = 0; j <= i; j++) {
+        string += '*';
+   }
+   string += '\n';
+}
+console.log(string);
 // ----------------------------------- //
 // let string2 = "";
 // for (let i = 0; i < 5; i++) {
