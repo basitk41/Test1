69e5fefd4e94eb6bb029fdf79bbca154688efcc7
Basit Ali <basitk41@gmail.com>
2022-07-18 11:40:13 +0500

    queue and stack

--- index.js
+++ index.js
@@ -22,22 +22,61 @@
 // queue => FIFO
 
 // length => stack.length
-const stack = [];
-const push = (value) => {
-  stack[stack.length] = value;
+// const stack = [];
+// const push = (value) => {
+//   stack[stack.length] = value;
+// };
+// const pop = () => {
+//   if (stack.length === 0) return "stack is empty!";
+//   const el = stack[stack.length - 1];
+//   stack.length--;
+//   return el;
+// };
+// push(1);
+// push(2);
+// push(3);
+// console.log(stack);
+// console.log(pop());
+// console.log(pop());
+// console.log(pop());
+// console.log(pop());
+// console.log(stack);
+
+// const input = [1, 2, 6, 3]; // original array
+// const output = []; // output empty array
+// for (let i = input.length - 1; i >= 0; i--) {
+//   output[output.length] = input[i];
+// }
+// console.log(output);
+
+const reverseArr = (input) => {
+  const output = [];
+  for (let i = input.length - 1; i >= 0; i--) {
+    output[output.length] = input[i];
+  }
+  return output;
 };
+
+let queue = [];
+
+const push = (value) => {
+  queue[queue.length] = value;
+};
+
 const pop = () => {
-  if (stack.length === 0) return "stack is empty!";
-  const el = stack[stack.length - 1];
-  stack.length--;
+  if (queue.length === 0) return "queue is empty!";
+  const el = queue[0];
+  const newQueue = reverseArr(queue);
+  newQueue.length--;
+  queue = reverseArr(newQueue);
   return el;
 };
+
 push(1);
 push(2);
 push(3);
-console.log(stack);
+console.log(queue);
 console.log(pop());
 console.log(pop());
 console.log(pop());
-console.log(pop());
-console.log(stack);
+console.log(queue);
